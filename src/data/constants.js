3a3d320dcc9d2ea3d48b6c7b75b78c925cd53d30
Nihitha Reddy S <71009213+nihitha167@users.noreply.github.com>
2024-05-22 22:45:46 -0500

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -23,10 +23,10 @@ export const Bio = {
       "Programmer",
     ],
     description:
-      "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+      "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning, I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
     github: "https://github.com/nihitha167",
     resume:
-      "https://drive.google.com/file/d/14I0E0CxAED38OxxXS7MMI3dIimuCSid2/view?usp=sharing",
+      "https://drive.google.com/file/d/1l5Vzv-AeEhpBcw6jCS2Om2o2jEV44QDH/view?usp=sharing",
     linkedin: "https://www.linkedin.com/in/nihitha-reddy-seelam/"
   };
   
